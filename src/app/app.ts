ed3185bea4e637718e04e622f24f8cd4474d3571
Josh Grajam Falcón Cabra <jgcarwayfc@gmail.com>
2025-08-25 16:49:45 -0500

    iniciado el sidenav, header, footer

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from "./header/header";
+import { Body } from "./body/body";
+import { Footer } from "./footer/footer";
+import { Sidenav } from "./sidenav/sidenav";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header, Body, Footer, Sidenav],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
