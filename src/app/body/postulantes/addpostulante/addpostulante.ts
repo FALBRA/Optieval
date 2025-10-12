4dbd1503a81d7c27ea1e01700417d89101faf201
Josh Grajam Falcón Cabra <jgcarwayfc@gmail.com>
2025-10-12 13:02:13 -0500

    agregado validador en addpostulante

--- src/app/body/postulantes/addpostulante/addpostulante.ts
+++ src/app/body/postulantes/addpostulante/addpostulante.ts
@@ -4,7 +4,7 @@ import { NgIf } from "@angular/common";
 
 @Component({
   selector: 'app-addpostulante',
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, NgIf],
   templateUrl: './addpostulante.html',
   styleUrl: './addpostulante.css'
 })
@@ -13,5 +13,30 @@ export class Addpostulante {
 
   cerrar() {
     this.cerrarEvent.emit();
+  }
+
+  addPostulanteForm: FormGroup;
+
+  constructor(private fb: FormBuilder){
+    this.addPostulanteForm = this.fb.group({
+      exoneracionPostulante: ['',[Validators.required]],
+      montoPostulante: ['',[Validators.required]],
+      modoPostulante: ['', [Validators.required]],
+      dniPostulante: ['', [Validators.required]],
+      namepostulante: ['', [Validators.required]],
+      lastnameFatherPostulante: ['', [Validators.required]],
+      lastnameMotherPostulante: ['', [Validators.required]],
+      carreraPostulante: ['', [Validators.required]],
+      egresoPostualante: ['', [Validators.required]],
+      schoolPostulante: ['', [Validators.required]]
+    })
+  }
+
+  onSubmit(){
+    if (this.addPostulanteForm.valid){
+      console.log('Datos:', this.addPostulanteForm.value);
+    } else {
+      this.addPostulanteForm.markAllAsTouched();
+    }
   }
 }
